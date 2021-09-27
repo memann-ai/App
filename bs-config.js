16986761b9079fff8050f8be3a4fd8d9455cc912
user_1 <memann@hotmail.co.jp>
2021-09-27 17:26:16 +0900

    調整

---
+++ bs-config.js
@@ -0,0 +1,19 @@
+
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+    "files": ["**/*.html, **/*.pug", "**/*.css", "**/*.js"],
+    "proxy": "http://localhost:3000",
+    "port": 4000,
+};
--- public/javascripts/hamburger.js
+++ public/javascripts/hamburger.js
@@ -4,14 +4,23 @@
     const open = document.getElementById("open");
     const overlay = document.querySelector(".overlay");
     const close = document.getElementById("close");
+    const mask = document.getElementById("mask");
 
     open.addEventListener("click", () => {
         console.log("クリック");
         overlay.classList.add("show");
+        overlay.classList.remove("hidden");
+        mask.classList.remove("hidden");
     });
 
     close.addEventListener("click", () => {
         console.log("closeをクリック");
         overlay.classList.remove("show");
+        overlay.classList.add("hidden");
+        mask.classList.add("hidden");
+    });
+
+    mask.addEventListener("click", () => {
+        close.click();
     });
 }
